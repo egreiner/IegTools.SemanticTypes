3934a8c4c960cf34c5737a4e467c468d175e6004
egreiner <github.ieg@ieg-tools.net>
2025-08-02 15:56:45 +0200

    Add IdentityValue type and corresponding unit tests

---
+++ src/IegTools.SemanticTypes/IdentityValue.cs
@@ -0,0 +1,14 @@
+﻿namespace IegTools.SemanticTypes;
+
+/// <summary>
+/// A semantic type that represents an identity value.
+/// This type is used to encapsulate a value that represents an identity, such as a user ID, product ID, etc.
+/// It is intended to provide type safety and clarity in the codebase.
+/// The underlying value can be of any type, such as int, string, Guid, etc.
+/// The value is immutable and can be used in comparisons and other operations as needed.
+/// </summary>
+public readonly record struct IdentityValue<T>(T Value)
+{
+    /// <inheritdoc />
+    public override string ToString() => Value.ToString();
+}
---
+++ tests/UnitTests.SemanticTypes/Identity/IdentityTests.cs
@@ -0,0 +1,52 @@
+﻿namespace UnitTests.SemanticTypes.Identity;
+
+using IegTools.SemanticTypes;
+
+public class IdentityTests
+{
+    [Fact]
+    public void Identities_ShouldBe_equal_when_same_int_values()
+    {
+        var id1 = new IdentityValue<int>(1);
+        var id2 = new IdentityValue<int>(1);
+
+        id2.Should().Be(id1);
+    }
+    
+    [Fact]
+    public void Identities_ShouldBe_equal_when_same_values()
+    {
+        var id1 = new IdentityValue<int>(1);
+        var id2 = new IdentityValue<int>(1);
+
+        var actual = id1 == id2;
+        actual.Should().BeTrue();
+    }
+    
+    [Fact]
+    public void Identities_ShouldBe_equal_when_same_string_values()
+    {
+        var id1 = new IdentityValue<string>("1");
+        var id2 = new IdentityValue<string>("1");
+
+        id2.Should().Be(id1);
+    }
+    
+    [Fact]
+    public void Identities_ShouldNotBe_equal_when_different_value()
+    {
+        var id1 = new IdentityValue<int>(1);
+        var id2 = new IdentityValue<int>(2);
+
+        id2.Should().NotBe(id1);
+    }
+    
+    [Fact]
+    public void Identities_ShouldNotBe_equal_when_different_types()
+    {
+        var id1 = new IdentityValue<int>(1);
+        var id2 = new IdentityValue<string>("1");
+
+        id2.Should().NotBe(id1);
+    }
+}
